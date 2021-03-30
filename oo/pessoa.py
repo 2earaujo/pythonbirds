28e83f0cb3e0ad25ea2f3497a7aacf95293f09b1
2earaujo <elias.empresarial@gmail.com>
2021-03-30 09:32:16 -0300

    Cria classe e adiciona um metodo, onde metodo eh uma funcao dentro de uma classe

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,9 @@
 class Pessoa:
-    pass
+    def cumprimentar(self):
+        return f'Ola {id(self)}'
+
+if __name__ == '__main__':
+    p = Pessoa()
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar()) # "p" eh o primeiro parametro, se adicionar outro item, ele sera o segundo parametro

5ec9410c490361165286ad9588bb978c8f48f614
2earaujo <elias.empresarial@gmail.com>
2021-03-30 10:41:54 -0300

    Criado atributo nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,15 @@
 class Pessoa:
+# para criar atributos, eu uso o double init:
+    def __init__(self,nome=None, idade=25):
+        self.idade = idade
+        self.nome = nome
     def cumprimentar(self):
         return f'Ola {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
-    print(Pessoa.cumprimentar(p))
+    p = Pessoa('Luciano')
+    #print(Pessoa.cumprimentar(p))
     print(id(p))
-    print(p.cumprimentar()) # "p" eh o primeiro parametro, se adicionar outro item, ele sera o segundo parametro
+    print(p.cumprimentar(),p.nome,p.idade)
+# "p" eh o primeiro parametro, se adicionar outro item, ele sera o segundo parametro
+# O metodo eh um tipo de atributo da Classe,mas nao eh o unico!

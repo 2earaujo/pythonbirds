67aba87c0090a2281194bfc0c74014d6ea5f0fc1
2earaujo <elias.empresarial@gmail.com>
2021-03-31 10:23:40 -0300

    Criado e removido atributo dinamico de objeto do tipo pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -8,14 +8,24 @@ class Pessoa:
         return f'Ola {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa(nome='Luciano')
-    filho_1 = Pessoa(nome='Douglas')
-    filho_2 = Pessoa(nome='Matilde')
-        pai = Pessoa(filho_1,nome='Pai-1')
+    #luciano = Pessoa(nome='Luciano')
+    douglas = Pessoa(nome='Douglas')
+    renzo = Pessoa(nome='Renzo')
+    luciano = Pessoa(douglas ,nome='Luciano')
     #print(Pessoa.cumprimentar(p))
     #print(id(p))
-    print(pai.cumprimentar(),pai.nome,pai.idade)
-    for filho in pai.filhos:
-        print(f'Filho do {pai.nome} eh {filho.nome}')
+    print(f'{luciano.nome} ! Hoje voce possui {luciano.idade} anos')
+    for filho in luciano.filhos:
+        print(f'Filho do {luciano.nome} eh {filho.nome}')
+    #adicionando argumentos dinamicos num OBJETO ESPECIFICO
+    luciano.sobrenome='Ramalho'
+    print(f'O objeto {luciano.nome} agora tem sobrenome = {luciano.sobrenome} ! So esse objeto foi alterado')
+    #acessando os elementos do objeto
+    print(luciano.__dict__)
+    print(renzo.__dict__ ,' => Observe que esse objeto possui menor numero de elementos!')
+    print('Agora vou remover o atributo filho')
+    del luciano.filhos
+    print(luciano.__dict__)
+
 # "p" eh o primeiro parametro, se adicionar outro item, ele sera o segundo parametro
 # O metodo eh um tipo de atributo da Classe,mas nao eh o unico!
